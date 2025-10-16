2d7ea31bdeaa664262aa24a2e052ba4112e23f29
Celio <celiocazares22@gmail.com>
2025-10-15 21:30:11 -0300

    23. Finish rendering the PropertyForm fields

--- actions/property-actions.ts
+++ actions/property-actions.ts
@@ -3,7 +3,10 @@
 import { propertyDataSchema } from "@/validation/propertySchema";
 import z from "zod";
 
+// it was first used because the new-property-form file wasnt accepting this function in a use client enviroment
+
 export async function createProperty(data: z.infer<typeof propertyDataSchema>) {
   // seu código aqui
+  console.log("Data -->", { data });
   return null;
 }
--- app/admin-dashboard/new/new-property-form.tsx
+++ app/admin-dashboard/new/new-property-form.tsx
@@ -1,12 +1,27 @@
 "use client";
 
-import { createProperty } from "@/actions/property-actions";
+// import { createProperty } from "@/actions/property-actions";
 import PropertyForm from "@/components/property-form";
+import { propertyDataSchema } from "@/validation/propertySchema";
+import { PlusCircleIcon } from "lucide-react";
+import z from "zod";
 
 export default function NewPropertyForm() {
+  async function createProperty(data: z.infer<typeof propertyDataSchema>) {
+    // seu código aqui
+    console.log("Data -->", { data });
+    return null;
+  }
   return (
     <div>
-      <PropertyForm handleSubmit={createProperty} />
+      <PropertyForm
+        handleSubmit={createProperty}
+        submitButtonLabel={
+          <>
+            <PlusCircleIcon /> Create Property
+          </>
+        }
+      />
     </div>
   );
 }
